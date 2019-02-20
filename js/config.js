6e498acb04769fcdc0e94610d090964a3115b652
Eugene Sobolev <mr_w_e_b@mail.ru>
2019-02-20 23:36:11 +1100

    end

--- js/config.js
+++ js/config.js
@@ -1,1 +1,194 @@
 var APIKey=f393f3131f6d9ca2c4788ef479d54149;
+
+// 1)
+// Name: School of International Education and Dormitory for International Students
+// Working Time: 8:00-12:00;14:30-18:00
+// Short Information: The dormitory for foreign students with cozy rooms. The 11th floor is equipped for married couples. On the first floor there is a small shop which works from 8:00 till 00:00. Department of the international education located on the 12th floor.
+// Address: 甘肃兰州市安宁区安宁西路88号19公寓，730070 （19, 88 West Anning Road, Anning, Lanzhou, Gansu, Post Code 730070 )
+// Coordinates: 36.109406, 103.727002
+// 2)
+// Name: Construction Bank
+// Working Time: 8:00-17:00
+// Short Information: Department of Construction Bank in which it is possible to make the bank card. And also there are ATMs in which it is possible to put or draw cash from account at any time.
+// Address: 甘肃兰州市安宁区兰州交通大学铁院新村87号楼 (87, Iron new residential quarter, Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.109780, 103.724524
+// 3)
+// Name: Student's Cafeteria
+// Working Time: 10:00-14:30;16:30-20:00
+// Short Information: Student's Cafeteria, in the north side of the first floor is a halal part, with beef noodles, lamb noodles, fast food. Non-halal in the south side, is also a fast food. The second floor has a variety of noodles and rice. The third floor is mainly cooking of the different meals. The school cafeteria is pretty cheap and affordable, also tastes good.
+// Address:甘肃兰州市安宁区兰州交通大学14号公寓 (14, Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.110102, 103.723580
+// 4)
+// Name: Library
+// Working Time: 8:00-16:30
+// Short Information: Founded in 1958, is the school's Information and documentation centre, a teaching and research services, academic institutions and the school is an important base of education and scientific research. Library has around 2,292,000 books. Convenience to read for the Jiaotong University students.
+// Address: 甘肃兰州市安宁区兰州交通大学内从大门口直走 (Near the main gate, Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.105680, 103.722914
+// 5) 
+// Name: Assembly Hall
+// Working Time: Festivals, concerts, university actions
+// Short Information: All university actions, concerts and other are held here.
+// Address: 甘肃兰州市安宁区兰州交通大学内大门口右拐 (Turn right from main gate, Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.103477, 103.722775
+// 6)
+// Name: Administrative building
+// Working Time: 8:00-12:00;14:30-18:00
+// Short Information: In this building is located the student's department, an office of the rector and all administrative structure of the university. Also in this building there are offices for independent work of students of some schools and an audience for some extracurricular activities.
+// Address: 兰州市安宁区兰州交通大学内行政楼 (The administration building, Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.103346, 103.724395
+// 7)
+// Name: Stadium
+// Working Time: 24 hours a day
+// Short Information: Here you can run, play football or just do physical exercises.
+// Address: 甘肃兰州市安宁区智扬路与棋荫路交叉口西100米(From the crossing Zhiyang and Qiyin Road to the west of 100 m., Anning, Lanzhou, Gansu)
+// Coordinates: 36.107795, 103.720543
+// 8) 
+// Name: Gym
+// Working Time: 8:00-21:00
+// Short Information: Here you can do physical exercise, keep your body in good shape at any time of the year.
+// Address: 甘肃兰州市安宁区兰州交通大学内图书馆左拐(Turn left from library, Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.106289, 103.721831
+// 9)
+// Name: Supermarket
+// Working Time: 8:00-23:00
+// Short Information: A small supermarket where you can buy food, stationery and some household items. You can also print, copy, or scan documents here.
+// Address: 甘肃兰州市安宁区兰州交通大学铁院新村87号楼 (87, Iron new residential quarter, Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.110007, 103.724373
+// 10)
+// Name: Information Center
+// Working Time: 8:00-12:00;14:30-18:00
+// Short Information: Here you can make a student card with which you can visit the stadium, library and other. Also with its help it is possible to pay for a meal in the Student's Cafeteria. Here you can put money on it. Even here you can spend the Internet to the dormitory or put money on it.
+// Address: 甘肃兰州市安宁区兰州交通大学内图书馆右拐(Turn right from library, Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.105984, 103.724438
+// 11)
+// Name: Cafeteria
+// Working Time: 10:00-20:00
+// Short Information: A small cafeteria which is located on the second floor. Mainly a varied selection of dishes with rice or noodles.
+// Address: 甘肃兰州市安宁区智扬路与棋荫路交叉口东北50米(From the crossing Zhiyang and Qiyin Road to the northeast of 50 m., Anning, Lanzhou, Gansu)
+// Coordinates: 36.108435, 103.721863
+// 12)
+// Name: Cafe
+// Working Time: 10:00-20:30
+// Short Information: A small cafe where you can drink a delicious drink or eat ice cream. There is also a small selection of fast food.
+// Address: 甘肃兰州市安宁区智扬路与棋荫路交叉口东北60米(From the crossing Zhiyang and Qiyin Road to the northeast of 60 m., Anning, Lanzhou, Gansu)
+// Coordinates: 36.108435, 103.722104
+// 13)
+// Name: Student's Cafeteria
+// Working Time: 10:00-14:30;16:30-20:00
+// Short Information: Limited choice of food, not too cleanly. It isn't recommended.
+// Address: 甘肃兰州市安宁区兴安路与学府路交叉口东南150米(From the crossing Xingan and Xuefu Road to the southeast of 150 m., Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.105523, 103.719664
+// 14)
+// Name: Basketball Court
+// Working Time: 24 hours a day
+// Short Information: On this court you can practise or play basketball.
+// Address: 甘肃兰州市安宁区智扬路与棋荫路交叉口西南150米(From the crossing Zhiyang and Qiyin Road to the southwest of 150 m., Anning, Lanzhou, Gansu)
+// Coordinates: 36.107869, 103.721294
+// 15)
+// Name: Volleyball Court
+// Working Time: 24 hours a day
+// Short Information: On this court you can practise or play volleyball.
+// Address: 甘肃兰州市安宁区兰州交通大学内体育馆后面(Behind gym, Inside in Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.107521, 103.722201
+// 16)
+// Name: West Gate
+// Working Time: 24 hours a day
+// Short Information: The west gate through which you can get to the territory of the University. Enterance by the cars allowed.
+// Address: 甘肃兰州市安宁区铁院新村1-114 (1-114, Iron new residential quarter, Anning, Lanzhou, Gansu)
+// Coordinates: 36.108849, 103.720120
+// 17)
+// Name: Main Gate
+// Working Time: 24 hours a day
+// Short Information: The main gate through which you can get to the territory of the University. Enterance by the cars unallowed.
+// Address: 甘肃兰州市安宁区安宁西路88号 (88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 36.103187, 103.722289
+// 18)
+// Name: Post Office (Students Express Service Centre)
+// Working Time: 9:00-18:30
+// Short Information: Main post center of the university. Here you can receive and send parcels. There is located offices of such express posts as: 申通快递，邮政EMS，顺丰速运，韵达快递，百世快递，汇通快递.
+// Address: 甘肃兰州市安宁区安宁西路88号兰州交通大学学生公寓12号楼 (12, Lanzhou Jiaotong University , 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 103.725199, 36.110333
+
+// 19)
+// Name: Post Office (圆通快递)
+// Working Time: 9:00-19:00
+// Short Information: There is located offices of圆通快递. Here you can receive and send parcels.
+// Address: 甘肃兰州市安宁区智扬路与棋荫路交叉口东北60米,从后面的入口(Entrance from the back, from the crossing Zhiyang and Qiyin Road to the northeast of 60 m., Anning, Lanzhou, Gansu)
+// Coordinates: 103.724779, 36.107552
+
+// 20)
+// Name: Post Office(中通快递)
+// Working Time: 9:00-18:00
+// Short Information: There is located offices of中通快递. Here you can receive and send parcels.
+// Address: 甘肃兰州市安宁区安宁西路88号13公寓 (13, 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 103.725858, 36.108865
+
+// 21)
+// Name: Post Office (天天快递)
+// Working Time: 9:00-17:00
+// Short Information: There is located offices of天天快递. Here you can receive and send parcels.
+// Address: 甘肃兰州市安宁区安宁西路88号14公寓 (14, 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 103.725853, 36.108661
+
+// 22)
+// Name: Post Office (中国邮政取件)
+// Working Time: 10:00-19:00
+// Short Information: There is located offices of中国邮政取件. Here you can receive and send parcels.
+// Address: 甘肃兰州市安宁区安宁西路88号21公寓 (21, 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 103.726520, 36.108562
+
+// 23)
+// Name: Educational building №1
+// Working Time: 7:30-22:00
+// Short Information: In this educational building hold an examinations. Also, Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学第一教学楼 (First educational building, Lanzhou Jiaotong University , 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 36.104625, 103.721313
+// 24)
+//  Name: Educational building №2
+// Working Time: 7:30-22:00
+// Short Information: In this educational building hold an examinations. Also, Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学第二教学楼 (Second educational building, Lanzhou Jiaotong University , 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 36.105204, 103.721836
+// 25)
+// Name: Educational building №3
+// Working Time: 7:30-22:00
+// Short Information: In this educational building hold an examinations. Also, some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学第三教学楼 (Third educational building, Lanzhou Jiaotong University , 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 36.105890, 103.721675
+// 26) 
+// Name: Educational building №4
+// Working Time: 7:30-22:00
+// Short Information: Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学(兰州市市政监察大队安宁中队东) ((Municipal Supervision brigade Anning Squadron East) Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.104208, 103.723774
+// 27)
+// Name: Educational building №5
+// Working Time: 7:30-22:00
+// Short Information: Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学(兰州市市政监察大队安宁中队东) ((Municipal Supervision brigade Anning Squadron East) Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.104902, 103.723619
+// 28)
+//  Name: Educational building №6
+// Working Time: 7:30-22:00
+// Short Information: Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学(兰州市市政监察大队安宁中队东) ((Municipal Supervision brigade Anning Squadron East) Lanzhou Jiaotong University, Anning, Lanzhou, Gansu)
+// Coordinates: 36.105481, 103.724101
+// 29)
+// Name: Educational building №7
+// Working Time: 7:30-22:00
+// Short Information: Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区棋荫路与达美路交叉口西南150米 (From the crossing Qiying and Damei Road to the southwest of 150 m., Anning, Lanzhou, Gansu)
+
+// Coordinates: 36.107262, 103.723570
+// 30)
+//  Name: Educational building №8
+// Working Time: 7:30-22:00
+// Short Information: In this educational building, the School of International Education conducts classes on listening. Also, as a rule, there is an exam on the level of language(HSK). Some schools conducts lessons here.
+// Address: 甘肃兰州市安宁区兰州交通大学第八教学楼 (Eighth educational building, Lanzhou Jiaotong University , 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 36.104638, 103.725757
+// 31) 
+// Name: Educational building №9
+// Working Time: 7:30-22:00
+// Short Information: In this educational building spend most of the classes for masters, as well as almost all classes for foreign students which study Chinese language.
+// Address: 甘肃兰州市安宁区兰州交通大学第九教学楼 (Ninth educational building, Lanzhou Jiaotong University , 88 West Anning Road, Anning, Lanzhou, Gansu)
+// Coordinates: 36.110554, 103.727439 
---
+++ js/database.js
@@ -0,0 +1,12 @@
+var arrObjects = []; 
+
+arrObjects[0] = {
+    id: "0",
+    name: "Construction Bank",
+    andress_ch:"甘肃兰州市安宁区兰州交通大学铁院新村87号楼",
+    andress_en:"87, Iron new residential quarter, Lanzhou Jiaotong University, Anning, Lanzhou, Gansu",
+    latitude:"103.724524",
+    longitude:"36.109780",
+    path_img:"img/code.png"
+}
+
